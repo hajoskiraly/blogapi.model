d09df096e0502e0c5a93fa069e9a78b610bf1c8a
HajosBB <hajosb@kkszki.hu>
2026-09-17 08:11:28 +0200

    Add project files.

---
+++ blogapi.model/Models/Blogger.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blogapi.model.Models
+{
+    public class Blogger
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string? Email { get; set; }
+        public int Age { get; set; }
+        public string? Password { get; set; }
+        public DateTime RegistrationTime { get; set; }
+
+    }
+}
